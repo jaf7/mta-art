29e393f813088766465bc680d3c5cf616de4c4f5
jaf7 <janthonyfields@gmail.com>
2018-02-20 02:49:17 -0500

    change back controller, move back index.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    refresh_db
   	erb :index
   end
 
-  # get "/artworks" do
-  #   refresh_db
-  #   erb :artworks
-  # end
+  get "/artworks" do
+    refresh_db
+    erb :artworks
+  end
 
   def refresh_db
     mta_art_catalogue_url = "https://data.ny.gov/resource/qius-v36q.json"
